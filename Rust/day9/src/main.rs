985fdb85265c8ac1811ffb8d26559afb5112fa2c
elsuizo <mnoblia@disroot.org>
2021-12-14 13:04:45 -0300

    update

--- Rust/day9/src/main.rs
+++ Rust/day9/src/main.rs
@@ -1,14 +1,14 @@
 use std::error::Error;
 use std::ops::{Index, IndexMut};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum AdjacencyType {
     Corner([u8; 2]),
     Normal([u8; 4]),
     Edge([u8; 3]),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Default)]
 struct Height {
     element: u8,
     adjacency_type: AdjacencyType,
@@ -30,11 +30,48 @@ struct HeightMap {
 }
 
 impl HeightMap {
-    fn init(rows: usize, cols: usize) -> Self {
-        Self {
+    fn init(rows: usize, cols: usize, input: &str) -> Self {
+        let mut result = Self {
             size: (rows, cols),
-            heights: Vec::with_capacity(rows * cols),
+            heights: vec![Height::default(); rows * cols],
+        };
+
+        for line in input.lines() {
+            for i in 0..rows {
+                for j in 0..cols {
+                    for num in line.chars().flat_map(|n| n.to_digit(10)) {
+                        println!("({}, {}): {:?}", i, j, result[(i, j)]);
+                        // match (i, j) {
+                        //     (0, col @ 1..=8) => {
+                        //         result[(i, col)] =
+                        //             Height::new(num as u8, AdjacencyType::Edge([0u8; 3]))
+                        //     }
+                        //     (5, col @ 1..=8) => {
+                        //         result[(i, col)] =
+                        //             Height::new(num as u8, AdjacencyType::Edge([0u8; 3]))
+                        //     }
+                        //     (row @ 1..=3, 0) => {
+                        //         result[(row, j)] =
+                        //             Height::new(num as u8, AdjacencyType::Edge([0u8; 3]))
+                        //     }
+                        //     (row @ 1..=3, 10) => {
+                        //         result[(row, j)] =
+                        //             Height::new(num as u8, AdjacencyType::Edge([0u8; 3]))
+                        //     }
+                        //     // (0, 0) | (0, 9) | (4, 0) | (4, 9) => {
+                        //     //     result[(i, j)] =
+                        //     //         Height::new(num as u8, AdjacencyType::Corner([0u8; 2]))
+                        //     // }
+                        //     (_, _) => {
+                        //         result[(i, j)] =
+                        //             Height::new(num as u8, AdjacencyType::Normal([0u8; 4]))
+                        //     }
+                        // }
+                    }
+                }
+            }
         }
+        result
     }
 }
 //-------------------------------------------------------------------------
@@ -60,31 +97,7 @@ impl IndexMut<(usize, usize)> for HeightMap {
 //-------------------------------------------------------------------------
 fn main() -> Result<(), Box<dyn Error>> {
     let input = include_str!("../input_small.txt");
-    let mut height_map = HeightMap::init(5, 10);
-    for line in input.lines() {
-        for i in 0..5 {
-            for j in 0..10 {
-                for num in line.chars().flat_map(|n| n.to_digit(10)) {
-                    match (i, j) {
-                        (i @ 1..9,) => {
-                            height_map[(i, j)] =
-                                Height::new(num as u8, AdjacencyType::Edge([0u8; 3]))
-                        }
-                        (_, 10) => {
-                            height_map[(i, j)] =
-                                Height::new(num as u8, AdjacencyType::Edge([0u8; 3]))
-                        }
-                        (0, 0) | (5, 0) | (5, 10) | (0, 10) => {
-                            height_map[(i, j)] =
-                                Height::new(num as u8, AdjacencyType::Corner([0u8; 2]))
-                        }
-                    }
-                }
-            }
-        }
-        // let height = Height::new(num as u8);
-        // height_map.heights.push(height);
-    }
+    let mut height_map = HeightMap::init(5, 10, input);
     println!("height_map: {:?}", height_map);
 
     Ok(())
